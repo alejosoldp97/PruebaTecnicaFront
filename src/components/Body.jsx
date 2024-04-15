b324341971612f0b628eb634b56ff4c5254ff597
alejosoldp97 <alejosoldp97@gmail.com>
2024-04-15 11:17:46 -0500

    ajustes a la tabla

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -24,7 +24,7 @@ const Body = ({
 }) => {
   return (
     <div>
-        <div className=' py-6'>
+        <div className=' py-6 ml-5 mr-5'>
         <PaginationComponent
           totalPages={totalPages}
           currentPage={currentPage}
